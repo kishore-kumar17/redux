9c91687cbca9e4d06c44cc2f2165aca2ff6137e2
kishore kumar <kishorekumar@dorustree.in>
2022-08-03 11:10:07 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import Add from "./redux/Add";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import View from "./redux/View";
 import Edit from "./redux/Edit";
-import Pages from "./Pages";
 import Counter from "./toolkit/Counter";
 import Create from "./redux crud/Create";
 import Viewcrud from "./redux crud/Viewcrud";
 import Editcrud from "./redux crud/Editcrud";
 import Home from "./socket io/Home";
 import Chatroom from "./socket io/Chatroom";
+import React from "react";
+
+
+
+// import Pages from "./Pages";
+// react lazyloading
+
+
+const Lazypart =React.lazy(()=>import('./Pages.js'));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Pages />}></Route>
+          {/* <Route path="/" element={<Pages />}></Route> */}
+
+
+          <Route path="/" element={<React.Suspense fallback={<h1 style={{color:'blue'}}>LOADING pleace wait.....</h1>}>< Lazypart/></React.Suspense>}></Route>
+
           <Route path="/add" element={<Add />}></Route>
           <Route path="view" element={<View />}></Route>
           <Route path="/edit/:id" element={<Edit />}></Route> 
